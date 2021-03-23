07b4be3ec6db119e8aa0a9eac2d5c7c55fe84f7e
ArturSlasher <archiblablablapicher@gmail.com>
2021-03-23 16:49:42 +0300

    made columns by myself

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+// RELOAD PAGE ON ctrl+shift+R
+
 // Enable the Quokka.js jsdom plugin and parse index.html
 
 // eslint-disable-next-line no-unused-expressions
@@ -5,8 +7,23 @@
 //   plugins: ['jsdom-quokka-plugin'],
 //   jsdom: { file: './index.html' },
 // });
-document.body.innerHTML = 'Hello!'
-export function test(x: number): number {
-  
-  return x + 1;
-}
+/*
+console.log('Hello!');
+const app: HTMLElement = document.getElementById('app') as HTMLElement;
+console.log(app);
+app.innerHTML = `<b>Hello Chess!</b>`;*/
+
+const app = document.getElementById('app') as HTMLElement;
+
+const range = (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
+
+const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].map( 
+    (letter) => range(8).map(number => ({letter:letter, number: number + 1}))
+);
+
+
+app.innerHTML = columns.map(
+    column => (`<div clas="row">` +
+        column.map(square => `<div class="square"></div>`).join('') +
+    `</div>`)
+).join('');
