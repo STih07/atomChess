8a87eb9f9bc0bfefe569a678fffc8f7d7c9cfffc
ArturSlasher <archiblablablapicher@gmail.com>
2021-03-23 18:44:28 +0300

    lesson with infosquares

--- src/app.ts
+++ src/app.ts
@@ -14,16 +14,26 @@ console.log(app);
 app.innerHTML = `<b>Hello Chess!</b>`;*/
 
 const app = document.getElementById('app') as HTMLElement;
-
 const range = (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].map( 
-    (letter) => range(8).map(number => ({letter:letter, number: number + 1}))
+
+const rows = range(8).map( 
+    (number) => letters.map(letter => ({letter:letter, number: 8 - number}))
 );
 
-
-app.innerHTML = columns.map(
-    column => (`<div clas="row">` +
-        column.map(square => `<div class="square"></div>`).join('') +
-    `</div>`)
-).join('');
+app.innerHTML = 
+    rows.map(
+        (row, index) => (`<div class="row">
+                        <div class="infosquare">${(rows.length - index)}</div>
+                        ${row.map(square => `<div class="square" id="${square.letter + square.number}"></div>`).join('')}
+                    </div>`)
+    ).join('')
+    +
+    `<div class="row">
+    <div class="infosquare"></div>
+        ${letters.map(
+            letter => `<div class="infosquare">` + letter + `</div>`
+        ).join('')}
+    </div>`;
+    
