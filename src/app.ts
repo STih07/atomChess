4ff16d8f58c757b30cf3c7203932c0a0cf74f741
Serhii <artur.momo@gmail.com>
2021-03-23 18:42:23 +0200

    Add Chess Board with Squares with IDs from js & Styles

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,35 @@
 //   jsdom: { file: './index.html' },
 // });
 
-export function test(x: number): number {
-  return x + 1;
-}
+// document.body.innerHTML = `<b>Hello</b>`;
+
+const app = document.getElementById('app') as HTMLElement;
+
+const range = (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const rows = range(8).map(
+    (number) => letters.map(
+        letter => ({letter: letter, number: 8 - number}))
+);
+
+
+app.innerHTML = 
+    rows.map(
+    (row, index) => (
+        `<div class="row"> 
+            <div class="infosquare">${row.length - index}</div> 
+            ${row.map(
+                square => `<div class="square" id="${square.letter + square.number}"></div>`
+            ).join('')} 
+        </div>`
+        )
+    ).join('')
+    +
+    `<div class="row">
+        <div class="infosquare"></div>
+        ${letters.map(
+            letter => `<div class="infosquare">${letter}</div>`
+        ).join('')}
+    </div>`;
