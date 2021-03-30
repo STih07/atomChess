1d15d895ed8e53befeab37c87dcb0c66ba78e503
Karim <khazanec@gmail.com>
2021-03-30 11:38:11 +0300

    Added Chess Board With Id`s and Styles

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,35 @@
 //   jsdom: { file: './index.html' },
 // });
 
-export function test(x: number): number {
-  return x + 1;
-}
-
 const app = document.getElementById('app') as HTMLElement;
-console.log(app);
-app.innerHTML = `<b>HELLO CHESS!</b>`;
+
+const range = (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const rows = range(8).map(
+    (number) => letters.map(
+         letter => ({letter: letter, number: 8 - number}))
+);
+
+
+app.innerHTML =
+    rows.map(
+    (row, index) => (
+        `<div class="row">
+            <div class="infosquare">${row.length - index}</div>
+            ${row.map(
+                square => `<div class="square" id="${square.letter + square.number}"></div>`
+            ).join('')}
+        </div>`
+        )
+    ).join('')
+    +
+    `<div class="row">
+        <div class="infosquare"></div>
+        ${letters.map(
+            letter => `<div class="infosquare">${letter}</div>`
+        ).join('')}
+    </div>`;
+          
+  
