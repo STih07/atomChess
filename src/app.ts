13c5986f6b5414dcabdeec71699d50ac9545526f
Savchenko <ss4vchenk0@gmail.com>
2021-04-06 21:37:00 +0300

    Savchenko

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,38 @@
-// Enable the Quokka.js jsdom plugin and parse index.html
 
-// eslint-disable-next-line no-unused-expressions
-// ({
-//   plugins: ['jsdom-quokka-plugin'],
-//   jsdom: { file: './index.html' },
-// });
+const app = document.getElementById('app') as HTMLElement;
 
-export function test(x: number): number {
-  return x + 1;
-}
+const range = (amout:number): Array<number> => Array.from<number>(Array(amout).keys());
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const rows = range(8).map(
+    (number)=>letters.map(
+        letter => ({letter: letter, number: 8 - number})
+    )
+);
+
+app.innerHTML = 
+
+        rows.map(
+            (row,index)=>(
+                `<div class="row">
+                    <div class = "infosquare">${row.length - index}</div>
+                    ${row.map(
+                        square => `<div class="square" id="${square.letter +square.number}"></div> `
+                    ).join('')}
+                </div>`      
+            )
+        ).join(' ')
+        +
+        `<div class="row">
+            <div class="infosquare"></div>
+            ${letters.map(
+                letter=>`<div class="infosquare">${letter}</div>`
+            ).join('')}
+           </div>`;
+
+//document.getElementsByClassName('piece');
+
+
+
+   
