3ddf95b2d31d02b60c61fe9f85c94152c3df5a8e
ArturSlasher <archiblablablapicher@gmail.com>
2021-04-18 18:13:58 +0300

    made 04-13 lesson

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,10 @@ enum Color {
     WHITE = 'w', 
     BLACK = 'b'
 }
+type ColoredPiece = {
+    piece: Piece,
+    color: Color
+}
 const app = document.getElementById('app') as HTMLElement;
 const range = (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -59,6 +63,15 @@ const positions: {[key: string]: string} = {
 const rows = range(8).map( 
     (number) => letters.map(letter => ({letter:letter, number: 8 - number}))
 );
+
+type Move = {
+    piece: ColoredPiece,
+    start: string,
+    end: string,
+    defeat: ColoredPiece | null
+}
+
+const moves: Array<Move> = [];
 
 app.innerHTML =
     rows.map(
@@ -124,25 +137,67 @@ const movePiece = (pieceRef: HTMLElement, placeBlock: HTMLElement) =>{
     const [, color, piece] = pieceRef.classList; // [piece w pawn]
     
     if (pieceMoveAllowed(piece as Piece, color as Color, pieceRef, placeBlock)){
+        moves.push({
+            piece: {
+                piece: piece as Piece,
+                color: color as Color
+            },
+            start: pieceRef.parentElement?.id as string,
+            end: pieceRef.parentElement?.id as string,
+            defeat: placeBlock.lastElementChild ? {
+                piece: placeBlock.lastElementChild?.classList[2] as Piece,
+                color: placeBlock.lastElementChild?.classList[1] as Color
+            } : null
+        })
+        placeBlock.innerHTML = '';
         placeBlock.appendChild(pieceRef);
     }
 }
 
-const isEndPositionClosedByFriend = (endPositionRef: HTMLElement, color:Color) => {
+const isEndPositionClosedByPiece = (endPositionRef: HTMLElement, color:Color, enemy:boolean) => {
     const child = endPositionRef.lastElementChild;
-    return child && child.classList.contains(color);
+    return child && (child.classList.contains(color) !== enemy);
 }
 
 const pieceMoveAllowed = (piece: Piece, color: Color, startPositionRef: HTMLElement, endPositionRef: HTMLElement) => {
     const startPosition = startPositionRef.parentElement?.id as string;
     const endPosition = endPositionRef.id;
     
-    if (!isEndPositionClosedByFriend(endPositionRef, color)){
+    if (!isEndPositionClosedByPiece(endPositionRef, color, false)){
+        const diffByY = +startPosition[1] - +endPosition[1]; //0
+        const diffByX = letters.indexOf(startPosition[0]) - letters.indexOf(endPosition[0]);
+        
         switch (piece) {
             case 'rook':
-                return (startPosition[0] === endPosition[0] || startPosition[1] === endPosition[1])
+                return (diffByY === 0 || diffByX === 0)
             case 'bishop':
-                return (startPosition[0] === endPosition[0] || startPosition[1] === endPosition[1])
+                return Math.abs(diffByY) === Math.abs(diffByX);
+            case 'knight':
+                const diffByCords = Math.abs(Math.abs(diffByY) - Math.abs(diffByX));
+                return (Math.abs(diffByY)===2 && Math.abs(diffByX)===1) || (Math.abs(diffByY)===1 && Math.abs(diffByX)===2);
+                //return diffByY <= 2 && diffByX <=2 && diffByCords === 1; 
+            case 'king':
+                return Math.abs(diffByX) <= 1 && Math.abs(diffByY) <= 1;
+            case 'queen':
+                return (diffByY === 0 || diffByX === 0) || (Math.abs(diffByY) === Math.abs(diffByX));
+            case 'pawn':
+                const firstStartPos = color === Color.WHITE ? '2' : '7';
+                const firstEndPos = color === Color.WHITE ? '4' : '5';
+                const oneForward = color === Color.WHITE ? -1 : 1;
+                const firstMoveOnTwo = diffByX === 0 && startPosition[1] === firstStartPos && endPosition[1] === firstEndPos; 
+                const newPosHasEnemy = isEndPositionClosedByPiece(endPositionRef, color, true);
+                const newPosIsEmpty = isEndPositionClosedByPiece(endPositionRef, color, true) === null;
+
+                const lastMove = moves[moves.length -1]
+                const lastIsPassant = lastMove && lastMove.piece.piece === 'pawn' && 
+                                    lastMove.start[2] === firstStartPos && 
+                                    lastMove.end[1] === firstEndPos;
+                //return firstMoveOnTwo || (diffByX === 0 && (newPosIsEmpty && !newPosHasEnemy) && diffByY === 1);
+                // ============================================
+                return (diffByX === 0 && (!newPosHasEnemy || newPosIsEmpty) && (firstMoveOnTwo || diffByY === oneForward)) ||
+                        Math.abs(diffByX) === 1 && ((newPosHasEnemy && !newPosIsEmpty) && diffByY === oneForward) 
+                            //|| (diffByY === oneForward && lastIsPassant && )
+
             default:
                 return true;
         }
