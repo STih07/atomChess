f5015b567870def5fef03530fde312b63477e35c
Daniil-Lobko <cheerfulgum@gmail.com>
2021-04-20 16:20:42 +0300

    m

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+const app = document.getElementById('app') as HTMLElement;
 
+const range= (amount: number): Array<number> => Array.from<number>(Array(amount).keys());
 
-document.body.innerHTML='Hello!';
+const letters = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h'];
+
+const rows = range(8).map(
+    (number) => letters.map(
+    letter => ({letter: letter, number: 8 - number }))
+); 
+
+app.innerHTML=
+        rows.map(
+            (row,index) => (
+                `<div class= "row"> 
+                    <div class = "infosquare">${row.length - index} </div>
+                        ${row.map(
+                        square => `<div class= "square" id="${square.letter + square.number}"></div>`
+                        ).join('')}
+                    </div>`
+            )
+         ).join('')
+         +
+         `<div class = "row"> <div class = "infosquare"> </div>
+         ${letters.map(
+             letter => `<div class = "infosquare">${letter}</div>`
+            ).join('')}
+            </div>`;
+
